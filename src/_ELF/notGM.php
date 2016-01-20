4df295e502df85c8894b05b0bc03cb5775a26764
엘F ㅣ ELF <junsuh911@naver.com>
2016-01-20 09:49:33 +0900

    Update notGM.php

--- src/_ELF/notGM.php
+++ src/_ELF/notGM.php
@@ -1,1 +1,24 @@
+<?php 
 
+namespace _Elf\notGM;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerGameModeChangeEvent;
+
+class notGM extends PluginBase implements Listener {
+	public function onEnable() {
+		$this->getLogger()->info(TextFormat::DARK_AQUA . "notGM has been Enable!");
+		$this->getLogger()->info(TextFormat::DARK_AQUA . "notGM's version is v1.0.1 beta");
+		$this->getLogger()->info(TextFormat::DARK_AQUA . "you have to remember! it is beta!");
+		
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$commandmap = $this->getServer()->getCommandMap();
+    }
+    public function notGM(PlayerGameModeChangeEvent $event) {
+    	$event->getPlayer()->kick("당신의 게임모드가 변경되었습니다 , OP가 아니므로 킥합니다")
+        if ($event->getPlayer()->isOp());
+    }
+    
+}
+?>
